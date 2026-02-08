7d9122d9f1f24db6089c1e22fc039d88ff135184
FAZE2000iQ <collinlovespiano@gmail.com>
2026-02-07 21:46:00 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let operator = null;
 let waitingForNewNumber = false;
 
 // 🔐 Secret unlock code
-const SECRET_CODE = "9408";
+const SECRET_CODE = "94087";
 
 function updateDisplay() {
   display.textContent = current;
